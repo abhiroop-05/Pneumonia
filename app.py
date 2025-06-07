6cc63495a391d67e56488fec41edfd1ce72873d1
abhiroop-05 <117287662+abhiroop-05@users.noreply.github.com>
2025-06-07 21:25:33 +0530

    app.py

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
 from flask import Flask, render_template, request
 from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing import image
 import numpy as np
-import os
+from PIL import Image
+from io import BytesIO
 
 app = Flask(__name__)
 model = load_model("pneumonia_model_mobilenetv2.h5")
 
-def predict_image(file_path):
-    img = image.load_img(file_path, target_size=(150, 150))
-    img_tensor = image.img_to_array(img)
-    img_tensor = np.expand_dims(img_tensor, axis=0) / 255.
+def predict_image(file_stream):
+    img = Image.open(BytesIO(file_stream.read())).convert("RGB")
+    img = img.resize((150, 150))
+    img_array = np.array(img) / 255.0
+    img_tensor = np.expand_dims(img_array, axis=0)
     prob = model.predict(img_tensor)[0][0]
     label = "Pneumonia" if prob > 0.5 else "Normal"
     confidence = round(prob * 100, 2) if label == "Pneumonia" else round((1 - prob) * 100, 2)
@@ -19,26 +20,17 @@ def predict_image(file_path):
 @app.route('/', methods=['GET', 'POST'])
 def index():
     predictions = []
-    files_saved = []
-
     if request.method == 'POST':
         files = request.files.getlist('images')
         for file in files:
-            filepath = os.path.join('static', file.filename)
-            file.save(filepath)
-            label, confidence, prob = predict_image(filepath)
+            label, confidence, prob = predict_image(file)
             predictions.append({
                 'filename': file.filename,
                 'label': label,
                 'confidence': confidence,
                 'prob': round(prob, 4)
             })
-            files_saved.append(filepath)
-
-        return render_template('index.html',
-                               predictions=predictions,
-                               image_paths=files_saved)
-    return render_template('index.html')
+    return render_template('index.html', predictions=predictions)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
